0af137d4489cb90a47a02d9c433078a68def7f05
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-12 22:33:57 +0100

    added templates for index,song actions

--- lyrics/views.py
+++ lyrics/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 
 from lyrics.models import Lyric
 
 def index(request):
-    latest_songs = Lyric.objects.order_by('-created_at')[:5]
-    output = ", ".join([s.title for s in latest_songs])
-    return HttpResponse(output)
+    template = loader.get_template('lyrics/index.html')
+    context = Context({
+        'songs': Lyric.objects.order_by('-created_at')[:5]
+    })
+    return HttpResponse(template.render(context))
+
+def index(request, song_id):
+    template = loader.get_template('lyrics/song.html')
+    context = Context({
+        'song': Lyric.objects.get(id=song_id)
+    })
+    return HttpResponse(template.render(context))
