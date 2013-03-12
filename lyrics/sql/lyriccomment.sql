bc4139fb37f7c9dee7999d75edd7e5f1fa6f0daa
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-12 19:10:17 +0100

    [customsql] fixture data provided

---
+++ lyrics/sql/lyriccomment.sql
@@ -0,0 +1,2 @@
+INSERT INTO `lyric_comment` (`id`, `lyric_id`, `text`, `author`, `ip`, `created_at`)
+VALUES (NULL, '5', 'This song is fantastic!', 'tkoomzaaskz', '127.0.0.1', '2013-03-12 15:35:24');
