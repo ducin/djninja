bae159607c277188bfcab16b3f451b4bdbfd0793
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-12 19:49:10 +0100

    fetching data from the database

--- lyrics/urls.py
+++ lyrics/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from lyrics import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^()?P<song_id>\d+/$', views.index, name='song'),
 )
--- lyrics/views.py
+++ lyrics/views.py
@@ -1,4 +1,8 @@
 from django.http import HttpResponse
 
+from lyrics.models import Lyric
+
 def index(request):
-    return HttpResponse("This is index page")
+    latest_songs = Lyric.objects.order_by('-created_at')[:5]
+    output = ", ".join([s.title for s in latest_songs])
+    return HttpResponse(output)
