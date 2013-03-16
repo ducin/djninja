6d2a6cab2a60b7c78cbbe332811151fbcb23f253
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-16 12:49:05 +0100

    moved slider data from template to view

--- lyrics/views.py
+++ lyrics/views.py
@@ -2,7 +2,27 @@ from django.shortcuts import render, get_object_or_404
 from lyrics.models import Lyric
 
 def index(request):
-    return render(request, 'lyrics/index.html', {})
+    context = { 'items': [
+        {
+            'image': 'examples/slide-01.jpg',
+            'headline': 'Example headline.',
+            'lead': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+            'button': 'Sign up today',
+        },
+        {
+            'image': 'examples/slide-02.jpg',
+            'headline': 'Another example headline.',
+            'lead': 'Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
+            'button': 'Learn more',
+        },
+        {
+            'image': 'examples/slide-03.jpg',
+            'headline': 'One more for good measure.',
+            'lead': 'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+            'button': 'Browse gallery',
+        },
+    ]}
+    return render(request, 'lyrics/index.html', context)
 
 def jukebox(request):
     songs = Lyric.objects.order_by('-created_at')[:5]
