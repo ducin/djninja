573a79a7e6753706c825122bdd672776e9ccf081
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-03-19 00:04:52 +0100

    contextized view teplate parameters

--- articles/views.py
+++ articles/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404, render_to_response
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from articles.models import Article
+from django.template import Context
 
 def slider(request):
-    context = { 'articles': Article.objects.order_by('-created_at')[:5] }
+    context = Context ({ 'articles': Article.objects.order_by('-created_at')[:5] })
     return render(request, 'articles/slider.html', context)
 
 def detail(request, article_id):
-    context = { 'article': get_object_or_404(Article, pk=article_id) }
+    context = Context ({ 'article': get_object_or_404(Article, pk=article_id) })
     return render(request, 'articles/detail.html', context)
 
 def archive(request, page="1"):
@@ -19,4 +20,7 @@ def archive(request, page="1"):
         articles = paginator.page(1)
     except EmptyPage:
         articles = paginator.page(paginator.num_pages)
-    return render_to_response('articles/archive.html', {'articles': articles})
+    context = Context({
+        'articles': articles
+    })
+    return render_to_response('articles/archive.html', context)
